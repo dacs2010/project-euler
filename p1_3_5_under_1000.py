dfc811fdb9b01c596ada4de54e83a2b9b294e2a6
xXDuckXx <dacs2010@gmail.com>
2019-02-27 10:43:02 -0500

    small fix for p1

--- p1_3_5_under_1000.py
+++ p1_3_5_under_1000.py
@@ -6,7 +6,7 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 
 def find_mult():
-    x = range(1, 1000)
+    x = range(1000)
     num_list = []
 
     for i in x:
--- p3_largest_prime.py
+++ p3_largest_prime.py
@@ -7,4 +7,9 @@ What is the largest prime factor of the number 600851475143 ?
 
 
 def prime_finder():
-    
+    n = 300
+    # prime is a number divisible by itself and 1
+    while i > n, i ++:
+
+
+prime_finder()
