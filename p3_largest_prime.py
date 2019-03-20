61dc7e8a560eb6da8726aeea5433418e70260d22
xXDuckXx <dacs2010@gmail.com>
2019-03-19 22:29:55 -0700

    fixes largest prime

--- p3_largest_prime.py
+++ p3_largest_prime.py
@@ -7,9 +7,17 @@ What is the largest prime factor of the number 600851475143 ?
 
 
 def prime_finder():
-    n = 300
-    # prime is a number divisible by itself and 1
-    while i > n, i ++:
+    n = 600851475143
+    i = 2
+    
+    while n >= i:
+        if n % i == 0 and n != i:
+            n = n // i
+            i = 2
+        else:
+            i += 1
+
+    print(f'{n} largest prime... i think')
 
 
 prime_finder()
